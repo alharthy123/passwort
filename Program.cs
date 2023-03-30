fb9ebcd898beb08f1c872b6dfc55fba63db73ac7
轶桐 吴 <3420675312@qq.com>
2023-03-30 11:17:44 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace passwort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pwort;
+            bool bedingung = false;
+            Console.WriteLine("8 zeichen und mindestens 1 Großenbuchtaben");
+        passwort:
+            Console.Write("Pasworteingeben :");
+            pwort= Console.ReadLine();
+            if(pwort.Length<8)
+            {
+                Console.WriteLine("zu kurz!");
+                goto passwort;
+            }
+            else if (!HatUpperCase(pwort))
+            {
+                Console.WriteLine("kein großebuchtaben");
+                goto passwort;
+            }
+            else if (!HatDigit(pwort))
+            {
+                Console.WriteLine("kein zahl");
+                goto passwort;
+            }
+            else
+            {
+                Console.WriteLine("paswort korrekt");
+            }
+            Console.ReadLine();
+        }
+        static bool HatUpperCase(string s)
+        {
+            bool bedingung=false;
+            for(int i = 0; i < s.Length;i++)
+            {
+                if(char.IsUpper(s[i]))
+                    bedingung= true;
+            }
+            return bedingung;
+        }
+
+        static bool HatDigit(string s)
+        {
+            bool bedingung = false;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsDigit(s[i]))
+                   bedingung = true;
+            }     
+         return bedingung;
+
+        
+            
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("passwort")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("passwort")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("f7732545-edbb-4078-8f1d-13078a685c83")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
